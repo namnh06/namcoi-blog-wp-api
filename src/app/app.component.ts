fe95ac9201f7c71fce05b46ffb6a705bc5775126
namnh06 <namnh.developer@gmail.com>
2019-06-09 14:21:51 +0700

    Added HttpClient & trying wordpress REST API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'namcoi-blog-wp-api';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get('https://namcoi.com/wp-json/wp/v2/posts?per_page=1')
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
 }
